dbf88c91b19af0bed737f4a51f7bcc9817800692
Yordan <yordan_yordanov001@abv.bg>
2021-12-21 13:00:50 +0200

    solution

--- src/js/Application.js
+++ src/js/Application.js
@@ -18,10 +18,9 @@ export default class Application extends EventEmitter {
     p.addEventListener("click", () => {
       // use anime.js
       anime({
-        targets: scrollElement,
-        scrollTop: 0,
-        duration: 500,
-        easing: "easeInOutQuad",
+        targets: ".squares",
+        translateX: 500,
+        easing: "spring(1,30,50,20)",
       });
     });
   }
