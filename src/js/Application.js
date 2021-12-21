2a24b314147d07e2064500216506c91a2c14a952
Yordan <yordan_yordanov001@abv.bg>
2021-12-21 12:26:38 +0200

    fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,6 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
+
 export default class Application extends EventEmitter {
   static get events() {
     return {
@@ -12,5 +13,11 @@ export default class Application extends EventEmitter {
     this.init();
     this.emit(Application.events.READY);
   }
-  init() {}
+  init() {
+    const p = document
+      .querySelector("p")
+      .addEventListener("click", function () {
+        console.log(p);
+      });
+  }
 }
